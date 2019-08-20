1931901ec857d481596b42db8ec6137e16bcea8f
Alexander Pacha <alexander.pacha@gmail.com>
2019-08-20 18:44:57 +0200

    Cleaning up training script and adding separate class for data loading, which also allows to cache all the images and masks in memory for efficient data loading.

--- prepare_muscima_pp.py
+++ prepare_muscima_pp.py
@@ -1,5 +1,10 @@
 from omrdatasettools.downloaders.MuscimaPlusPlusDatasetDownloader import MuscimaPlusPlusDatasetDownloader
+from omrdatasettools.image_generators.MuscimaPlusPlusMaskImageGenerator import \
+    MuscimaPlusPlusMaskImageGenerator, MaskType
 
 if __name__ == '__main__':
     downloader = MuscimaPlusPlusDatasetDownloader(dataset_version="2.0")
-    downloader.download_and_extract_dataset("muscima-pp")
+    downloader.download_and_extract_dataset("muscima_pp")
+
+    mask_generator = MuscimaPlusPlusMaskImageGenerator()
+    mask_generator.render_node_masks("muscima_pp", "muscima_pp_masks", MaskType.STAFF_BLOBS_INSTANCE_SEGMENTATION)
